63d198ee033b2375538b69c45d3104acc4cb11a6
Jeff Hartley <jeffhartley3925@live.com>
2014-09-05 00:03:08 -0400

    Fixed corrupt EF Model files

--- Composer.Entities/Model.cs
+++ Composer.Entities/Model.cs
@@ -15,21 +15,21 @@ using System.Collections.Specialized;
 
 namespace Composer.Entities
 {
-    // An System.Collections.ObjectModel.ObservableCollection that raises
-    // individual item removal notifications on clear and prevents adding duplicates.
-    public class FixupCollection<T> : ObservableCollection<T>
-    {
-        protected override void ClearItems()
-        {
-            new List<T>(this).ForEach(t => Remove(t));
-        }
-    
-        protected override void InsertItem(int index, T item)
-        {
-            if (!this.Contains(item))
-            {
-                base.InsertItem(index, item);
-            }
-        }
-    }
+	// An System.Collections.ObjectModel.ObservableCollection that raises
+	// individual item removal notifications on clear and prevents adding duplicates.
+	public class FixupCollection<T> : ObservableCollection<T>
+	{
+		protected override void ClearItems()
+		{
+			new List<T>(this).ForEach(t => Remove(t));
+		}
+
+		protected override void InsertItem(int index, T item)
+		{
+			if (!this.Contains(item))
+			{
+				base.InsertItem(index, item);
+			}
+		}
+	}
 }
